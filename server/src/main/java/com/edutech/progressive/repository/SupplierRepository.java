cb8778f17eee2dec342cf694067a623653b2bc68
Rajkumar19029 <87936393+Rajkumar19029@users.noreply.github.com>
2026-02-01 00:47:49 +0530

    Update SupplierRepository.java

--- server/src/main/java/com/edutech/progressive/repository/SupplierRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/SupplierRepository.java
@@ -1,19 +1,18 @@
-package com.edutech.progressive.repository;
+package com.wecp.progressive.repository;
 
-
-
+import com.wecp.progressive.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
 
-import com.edutech.progressive.entity.Supplier;
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
 
-public interface SupplierRepository extends JpaRepository<Supplier,Integer>{
-    @Transactional
-    @Query("Delete from Supplier s where s.supplier_id=:supplierId")
     void deleteBySupplierId(@Param("supplierId") int supplierId);
 
-    @Query("Select s from Supplier s where s.supplier_id=:supplierId")
     Supplier findBySupplierId(@Param("supplierId") int supplierId);
+
+    Supplier findByUsername(String username);
+
+    Supplier findByEmail(String email);
 }
