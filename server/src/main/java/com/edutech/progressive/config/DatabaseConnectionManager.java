19696a64074482714306c80788d92572bb80a52a
Rajkumar19029 <87936393+Rajkumar19029@users.noreply.github.com>
2026-02-01 00:01:15 +0530

    Update DatabaseConnectionManager.java

--- server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
+++ server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
@@ -1,4 +1,6 @@
-package com.edutech.progressive.config;
+package com.wecp.progressive.config;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,29 +8,27 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseConnectionManager {
-    private static Properties properties=new Properties();
-    static{
+    private static final Properties properties = new Properties();
+
+    static {
         loadProperties();
     }
-    public static void loadProperties(){
-        try(InputStream input=DatabaseConnectionManager.class .getClassLoader().getResourceAsStream("application.properties")) {
-            if(input==null){
-                throw new RuntimeException("applications.properties file not found");
+
+    private static void loadProperties() {
+        try (InputStream input = DatabaseConnectionManager.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                throw new IllegalStateException("resource.properties not found in classpath");
             }
             properties.load(input);
-         
-        }catch(Exception e){
-            throw new RuntimeException("Failed to load database",e);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading properties file", e);
         }
     }
-    public static Connection getConnection() throws SQLException{
-        try{
-        String url=properties.getProperty("spring.datasource.url");
-        String username=properties.getProperty("spring.datasource.username");
-        String password=properties.getProperty("spring.datasource.password");
-        return DriverManager.getConnection(url, username, password);
-    }catch(Exception e){
-        throw new RuntimeException("Failed to establish database connection",e);
+
+    public static Connection getConnection() throws SQLException {
+        String url = properties.getProperty("spring.datasource.url");
+        String user = properties.getProperty("spring.datasource.username");
+        String password = properties.getProperty("spring.datasource.password");
+        return DriverManager.getConnection(url, user, password);
     }
-}
 }
