45a1c0596b2a2dfc4464f1235e7c3311e59a1f99
Rajkumar19029 <87936393+Rajkumar19029@users.noreply.github.com>
2026-02-01 12:05:20 +0530

    Update WarehouseRepository.java

--- server/src/main/java/com/edutech/progressive/repository/WarehouseRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/WarehouseRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edutech.progressive.entity.Warehouse;
 
-public interface WarehouseRepository extends JpaRepository<Warehouse,Integer>{
-    Warehouse findByWarehouseId(int warehouseId);
-    List<Warehouse> findAllBySupplier_SupplierId(int supplierId);
-    void deleteBySupplierId(int supplierId);
-    
+@Repository
+public interface WarehouseRepository extends JpaRepository<Warehouse, Integer> {
+    Warehouse findByWarehouseId(@Param("warehouseId") int warehouseId);
+
+    List<Warehouse> findAllBySupplier_SupplierId(@Param("supplierId") int supplierId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Warehouse w WHERE w.supplier.supplierId = :supplierId")
+    void deleteBySupplierId(@Param("supplierId") int supplierId);
 }
